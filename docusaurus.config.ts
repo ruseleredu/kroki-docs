aa9bb431ce3a5c52018b6b6c48b0ac094152fdc3
Adriano Ruseler <ruseler@utfpr.edu.br>
2026-07-27 22:17:12 -0300

    Add Future flags

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -11,7 +11,24 @@ const config: Config = {
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
-    v4: true, // Improve compatibility with the upcoming Docusaurus v4
+    v4: {
+      removeLegacyPostBuildHeadAttribute: true,
+      useCssCascadeLayers: true,
+      siteStorageNamespacing: true,
+      fasterByDefault: true,
+      mdx1CompatDisabledByDefault: true,
+    },
+    faster: {
+      swcJsLoader: true,
+      swcJsMinimizer: true,
+      swcHtmlMinimizer: true,
+      lightningCssMinimizer: true,
+      rspackBundler: true,
+      rspackPersistentCache: true,
+      ssgWorkerThreads: true,
+      mdxCrossCompilerCache: true,
+    },
+    // experimental_router: 'hash',
   },
 
   // Set the production url of your site here
